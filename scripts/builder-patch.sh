b626a7f545f4cc7bae582575e2a7681289d1543f
lambdaplus <bond010418@gmail.com>
2023-12-26 13:33:53 +0800

    Update builder-patch.sh

--- scripts/builder-patch.sh
+++ scripts/builder-patch.sh
@@ -5,6 +5,9 @@ echo "Current Path: $PWD"
 
 cd $GITHUB_WORKSPACE/$VENDOR-imagebuilder-$VERSION-x86-64.Linux-x86_64 || exit
 
+# fix bios boot partition is under 1 MiB
+sed -i 's/256/1024/g' target/linux/x86/image/Makefile
+
 # Remove redundant default packages
 sed -i "/luci-app-cpufreq/d" include/target.mk
 
@@ -16,3 +19,7 @@ sed -i "s/CONFIG_ISO_IMAGES=y/# CONFIG_ISO_IMAGES is not set/" .config
 
 # Not generate VHDX images
 sed -i "s/CONFIG_VHDX_IMAGES=y/# CONFIG_VHDX_IMAGES is not set/" .config
+
+# 分区改成 50/900
+sed -i "s/CONFIG_TARGET_KERNEL_PARTSIZE=32/CONFIG_TARGET_KERNEL_PARTSIZE=50/" .config
+sed -i "s/CONFIG_TARGET_ROOTFS_PARTSIZE=300/CONFIG_TARGET_ROOTFS_PARTSIZE=900/" .config
