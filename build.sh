c3252775d7ba93087232e5a4136ff1c7569008d5
lambdaplus <bond010418@gmail.com>
2024-12-09 16:40:16 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -110,7 +110,7 @@ PACKAGES="$PACKAGES lrzsz"
 #PACKAGES="$PACKAGES netdata"
 
 # 一些自定义文件
-#FILES="files"
+FILES="files"
 
 # 禁用 openssh-server 的 sshd 服务和 docker 的 dockerd 服务以防止冲突
 DISABLED_SERVICES="dockerd"
