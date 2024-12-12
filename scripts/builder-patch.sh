4ebd05e741249b3afca7a8341c764a303fd7a4af
lambdaplus <bond010418@gmail.com>
2024-12-12 09:49:47 +0800

    Update builder-patch.sh
    不掉虚拟机类型的镜像格式

--- scripts/builder-patch.sh
+++ scripts/builder-patch.sh
@@ -17,6 +17,15 @@ sed -i "s/install \$(BUILD_PACKAGES)/install \$(BUILD_PACKAGES) --force-overwrit
 # Not generate ISO images for it is too big
 sed -i "s/CONFIG_ISO_IMAGES=y/# CONFIG_ISO_IMAGES is not set/" .config
 
+# Not generate QCOW2 images
+sed -i "s/CONFIG_QCOW2_IMAGES=y/# CONFIG_QCOW2_IMAGES is not set/" .config
+
+# Not generate VDI images
+sed -i "s/CONFIG_VDI_IMAGES=y/# CONFIG_VDI_IMAGES is not set/" .config
+
+# Not generate VMDX images
+sed -i "s/CONFIG_VMDK_IMAGES=y/# CONFIG_VMDK_IMAGES is not set/" .config
+
 # Not generate VHDX images
 sed -i "s/CONFIG_VHDX_IMAGES=y/# CONFIG_VHDX_IMAGES is not set/" .config
 
