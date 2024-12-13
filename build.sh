4eb48d20ab65bd764cd790ec616a29c23f64dd9b
lambdaplus <bond010418@gmail.com>
2024-12-13 13:59:50 +0800

    Update build.sh
    rm socat

--- build.sh
+++ build.sh
@@ -50,7 +50,7 @@ PACKAGES="$PACKAGES luci-i18n-aria2-zh-cn"
 # FTP 服务器
 PACKAGES="$PACKAGES luci-i18n-vsftpd-zh-cn"
 # 端口转发
-PACKAGES="$PACKAGES luci-i18n-socat-zh-cn"
+# PACKAGES="$PACKAGES luci-i18n-socat-zh-cn"
 # TTYD
 PACKAGES="$PACKAGES luci-i18n-ttyd-zh-cn"
 # WechatPush
