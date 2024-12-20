9dfd666cd4c39e79ff937becf782a1ef78c094e2
lambdaplus <bond010418@gmail.com>
2024-12-20 16:08:17 +0800

    Update 24_10.sh
    编译出现错误 gowebdav

--- 24_10.sh
+++ 24_10.sh
@@ -39,8 +39,8 @@ PACKAGES="$PACKAGES luci-i18n-ddns-go-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-hd-idle-zh-cn"
 # p910nd 打印服务器
 PACKAGES="$PACKAGES luci-i18n-p910nd-zh-cn"
-# GoWebDav
-PACKAGES="$PACKAGES luci-i18n-gowebdav-zh-cn"
+# GoWebDav 编译出现错误
+# PACKAGES="$PACKAGES luci-i18n-gowebdav-zh-cn"
 # Samba 网络共享
 PACKAGES="$PACKAGES luci-i18n-samba4-zh-cn"
 # qBittorrent 下载器
